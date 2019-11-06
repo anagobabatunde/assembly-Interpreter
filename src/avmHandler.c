65b2a079ef454755a213e0e86ffed8cd9812c22c
ikf3 <contact@ikf3.com>
2019-11-06 11:56:02 +0100

    WIP some progess

--- src/avmHandler.c
+++ src/avmHandler.c
@@ -1,94 +1,23 @@
 #include "../lib/lib.h"
 
-
-char* appendChar(char* src, char *a) {
-    char *name = malloc(sizeof(char*));
-    // some code that puts something in *name
-    char d = a;
-    printf(src[0]);
-    printf("len is :");
-    printf(my_strlen(src));
-    printf("\n");
-    size_t len = strlen(src);
-    // if (len >= (80-1)) DealWithNotEnoughRoom(); 
-    name[len++] = d;
-    name[len] = '\0';
-    return name;
-}
-
-char* getLnByLnNum(char* src, int rl) {
-    char* des =  malloc(sizeof(char*) * 1024);
-    int i, line = 0;
-    printf("\ni at %d and line at %d.\n", i, line );
-    while (rl != line) {
-        i++;
-        if (src[i] == '\0' || src[i] == '\n') {
-            //printf("\n%s%d\n""new line at pos:", i);
-            line++;
-            i++;
-        }
-    }
-    for (i; src[i] != '\n' && i < 119; i++) {
-        if (src[i] != '\0' || src[i] != '\n') {
-            // printf("%c", src[i]);
-            appendChar(des, src[i]);
-            //printf("%c")
-            //size_t dLen = my_strlen(des);
-            //des[dLen] = src[i];
-            //printf("_%c", src[i]);
-            //des[dLen++] = '\0';
-            //printf("--- wsh ---\n");
-            //printf("%c", dat[k]);
-            //my_strcpy(tData, &dat[k]);
-        }
-        //printf(" %c" , src[i]);
-    }
-    i, line = 0;
-    //printf("\n%s%s-\n", "before out:", des);
-    char* tmp = my_strdup(des);
-    free(des);
-    return tmp;
-}
-
 void parser(char* dat, int readReturn) {
     int k,line, col = 0;
-    char* tBfr =  malloc(sizeof(char*) * 100);
+    size_t len = 0;
+    char* tBfr =  malloc(sizeof(char*));
     // tBfr[0] = '\0';
-    //strcpy(tBfr, "wsh");
-    /*for (k = 0; dat[k] != '\0'; k++) {
-        //tab[line][col] = buffer[k];
-        col++;
-        printf("%s", dat[k]);
-        if (dat[k] == '\n') {
-            line++;
-            printf("%s%s", dat[k], "-");
-            col = 0;
-        }
-    }*/
-    // tBfr = malloc(sizeof(dat) / sizeof(dat[0]));
     for (k = 0; dat[k] != '\0'; k++) {
-        if (dat[k] == '\n') {
-            //printf(tData);
-            //tBfr = dat[k];
-            //tData[0] = 0;
+        if (dat[k] == '\n' || dat[k] == '\0') {
+            if (notAlone(tBfr))
+                printf(" aye that guy not alone need to handle him ");
             printf("-nl-\n");
-            // tBfr = 0;
+            tBfr[0] = '\0';
         } else {
-            //printf("--- wsh ---\n");
-            // appendChar(tBfr, dat[k]);
-            printf("%c", dat[k]);
-            //my_strcpy(tData, &dat[k]);
+            len = my_strlen(tBfr);
+            tBfr[len++] = dat[k];
+            tBfr[len] = '\0';
         }
-        // printf("%c" , avm.data[k]);
     }
-    // char* yes;
-    // yes = my_strdup(dat);
-    // printf(dat);
-    // printf(getLnByLnNum(dat, 0));
     printf("\n----\n");
-    // printf(getLnByLnNum(yes, 1));
-    printf("\n----\n");
-    printf("%c\n", tBfr[0]);
     printf("%s\n", tBfr);
     free(tBfr);
     printf("-\n");
@@ -107,10 +36,10 @@ void handleAvm(adat_t **avm) {
     
 }
 
-int isAlone(char *str) {
+int notAlone(char *str) {
     int i = 0;
-    while (str[i] != '\n') {
-        if (str[i] != ' ')
+    while (str[i] != '\0') {
+        if (str[i] == ' ')
             return 1;
         i++;
     }
--- src/fileHandler.c
+++ src/fileHandler.c
@@ -1,18 +1,11 @@
 #include "../lib/lib.h"
 
-int isRegFile(const char *p) {
-    struct stat pt;
-    stat(p, &pt);
-    return S_ISREG(pt.st_mode);
-}
-
 adat_t exitAndMsg(char* msg, int code) {
     adat_t adat;
     adat.adt.msg = msg;
     adat.adt.ret = code;
     return adat;
 }
-
 
 int getNbLine(char *b) {
     int k, l = 0;
@@ -27,66 +20,41 @@ int getLine(char* path) {
     int in, rt;
     char buffer[1024];
     // rt = 0;
-
     in = open(path, O_RDONLY);
     rt = read(rt, buffer, 1023);
     buffer[rt] = '\0';
     close(in);
-
     return getNbLine(buffer);
 }
 
 adat_t getAvm(char* path) {
     adat_t adat;
     int in, size, rt;
-    /* RIP
-    char *ext = strrchr(path, '.');
-    if (! isRegFile(path) || ext == NULL || strcmp((ext + 1),"avm") != 0)
-       return exitAndMsg("Not a regular .avm file!", 1);
-    */
     in = open(path, O_RDONLY);
     if (in == -1)
         return exitAndMsg("Error while opening file. Please check filepath.", 1);
-    /* //The stat way:
-    char* bfr;
-    struct stat sz; // is stat allowed?
-    stat(path, &sz);
-    size = sz.st_size;
-    bfr = malloc(size * (sizeof(char)));
-    rt = read(in, bfr, size);
-    */
-    // The dégun way:
     // Andy idea: read 8bit per 8bit, then there will be a '\0'
-    //char bfr[1024];
+    /*
+    // Tonio impl:
+    int i = 0;
+    rt = 1;
+    char bfr[8];
+    while (rt != 0) {
+        while (i < 9)
+            bfr[i] = '\0'; i++;
+        rt = read(in, bfr, 8);
+        my_strcpy(adat.data, bfr); 
+    }
+    */
     char* bfr = malloc(sizeof(char *) * 1024);
-    //char* bfr;
-    //size = 1023;
-    //bfr = malloc(size * (sizeof(char)));
     rt = read(in, bfr, 1023);
     if (rt == 0 || rt == -1)
         return exitAndMsg("Error blank file?", 1);
-    // printf("%s%d\n","return is :", rt);
-    bfr[rt + 1] = '\0';
+    bfr[rt++] = '\n';
+    bfr[rt] = '\0';
     adat.data = bfr;
     close(in);
-     printf("read ret is :");
-     printf("%d\n", rt);
-     adat.read_ret = rt; // BUG push int32(42L
-    // adat.adt.ret = 0;
-    // free(bfr);
+    adat.read_ret = rt;
+    // free(bfr); // TODO FREE
     return adat;
-}
-
-int check_args(int c, char *v[]) {
-    if (c > 2)  {
-        if (!my_strcmp(v[2],"--hardcore"))
-            return (2);
-        else {
-            my_printf("E: Second argument should be \"--hardcore\" !\n"); 
-            return (1);
-        }
-    } else if (c < 2 || c > 3) {
-        return (2);
-    }
-    return 0;
 }
